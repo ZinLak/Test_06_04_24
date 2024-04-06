c5e80a710dd298115828015978646a7fc511c008
ZINLAK <108427946+ZinLak@users.noreply.github.com>
2024-04-06 10:13:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,3 @@
-def print_spiral(matrix):
-    top = left = 0
-    bottom = len(matrix) - 1
-    right = len(matrix[0]) - 1
-    
-    while left <= right and top <= bottom:
-        for i in range(left, right + 1):
-            print('\n', matrix[top][i], end=" ")
-            print('--', top, i, end=" ")
-        top += 1
-        
-        for i in range(top, bottom + 1):
-            print('\n', matrix[i][right], end=" ")
-            print('--', i, right, end=" ")
-        right -= 1
-        
-        if top <= bottom:
-            for i in range(right, left - 1, -1):
-                print('\n', matrix[bottom][i], end=" ")
-                print('--', bottom, i, end=" ")
-            bottom -= 1
-        
-        if left <= right:
-            for i in range(bottom, top - 1, -1):
-                print('\n', matrix[i][left], end=" ")
-                print('--', i, left, end=" ")
-            left += 1
-
 m = [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7],
      [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7],
      [2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7],
@@ -35,4 +7,29 @@ m = [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7],
      [6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7],
      [7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7]]
 
-print_spiral(m)
+top = left = 0
+bottom = len(m) - 1
+right = len(m[0]) - 1
+    
+while left <= right and top <= bottom:
+    for i in range(left, right + 1):
+        print('\n', m[top][i], end=" ")
+        print('--', top, i, end=" ")
+    top += 1
+        
+    for i in range(top, bottom + 1):
+        print('\n', m[i][right], end=" ")
+        print('--', i, right, end=" ")
+    right -= 1
+        
+    if top <= bottom:
+        for i in range(right, left - 1, -1):
+            print('\n', m[bottom][i], end=" ")
+            print('--', bottom, i, end=" ")
+        bottom -= 1
+        
+    if left <= right:
+        for i in range(bottom, top - 1, -1):
+            print('\n', m[i][left], end=" ")
+            print('--', i, left, end=" ")
+        left += 1
